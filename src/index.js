16a88b2c1a732e90309edc8422bc7facca2623eb
Artem Martyniuk <muartem99@gmail.com>
2021-12-19 04:15:06 +0200

    layout ended

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { createStore, applyMiddleware, compose } from 'redux'
+import { createStore, applyMiddleware } from 'redux'
 import { Provider } from 'react-redux'
 import thunk from 'redux-thunk'
 
@@ -13,13 +13,15 @@ import { GlobalStyle } from './assets/styles/globalStyle'
 
 const store = createStore(
   rootReducer,
+  applyMiddleware(
+    thunk
+  )/* ,
   compose(
-    applyMiddleware(
-      thunk
-    ),
+
     // eslint-disable-next-line no-underscore-dangle
+
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  )
+  ) */
 )
 
 ReactDOM.render(
@@ -32,7 +34,5 @@ ReactDOM.render(
   document.getElementById('root')
 )
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+
 reportWebVitals()
